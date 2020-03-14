ee590f6ca852cdb6b2e0e8e89b4fdc0c298ca682
Marcus Underwood <marcusunderwood0729@gmail.com>
2020-03-13 19:36:21 -0500

    Switching from .env to python config file.

---
+++ config.py
@@ -0,0 +1,10 @@
+client_id =     # Reddit Client App ID
+client_secret =     # Reddit Client App Secret
+user_agent =    # Reddit User Agent 
+activities =    # List of activities for bot status to cycle through
+discord_token =     # Bot discord token
+preset_terms = {
+    """
+    "term" : ["List of terms to check for when key inputted."]
+    """
+}
